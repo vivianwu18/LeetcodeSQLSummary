29b54e70fcff42defe631192be80d60bd75c2642
Meng-Wei Wu <123716809+vivianwu18@users.noreply.github.com>
2023-07-17 22:01:21 +0800

    delete

--- JOIN/184_Department_Highest_Salary.sql
+++
@@ -1,7 +0,0 @@
-WITH tmp AS (
-    SELECT departmentID, MAX(salary) OVER (PARTITION BY departmentID) AS max_salary
-    FROM Employee)
-SELECT DISTINCT d.name AS Department, e.name AS Employee, salary
-FROM Employee AS e
-JOIN Department AS d ON e.departmentID = d.id
-JOIN tmp ON e.salary = tmp.max_salary AND d.id = tmp.departmentId
--- JOIN/574_Winning_Candidate.sql
+++
@@ -1,5 +0,0 @@
-SELECT TOP 1 name
-FROM Vote AS v
-JOIN Candidate AS c ON v.candidateId = c.id
-GROUP BY name
-ORDER BY COUNT(v.id) DESC
